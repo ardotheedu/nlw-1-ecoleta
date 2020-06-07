2cadb3b108f3ebf23a121c20467bc34d04facbf4
ardotheedu <eduardosalima3@gmail.com>
2020-06-07 15:09:17 -0300

    src atualizado

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const server = express()
 
+const db = require("./database/db.js")
+
 // configurar pasta publica, tornar todos os arquivos na pasta public de forma publica
 // Assim, não é preciso colocar public/assests... ele ja vai esta disponivel
 
 server.use(express.static("public"))
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
 
 // utilizando template engine
 const nunjucks = require("nunjucks")
@@ -28,8 +33,63 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name, 
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err){
+        if(err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("cadastrado com sucesso")
+        console.log(this)
+        return res.render("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    const search = req.query.search
+
+    if(search == ""){
+        return res.render("search-results.html", {total: 0})
+    }
+
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+        if(err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        return res.render("search-results.html", {places: rows, total: total})
+    }) 
+
+    
 })
 
 
